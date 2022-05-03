cf98a38c741dfa0e85be3cce3a9241fcd942dd19
Jhonata Souza <einasota@gmail.com>
2022-05-03 15:38:27 -0300

    Finishing the second class

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,5 @@
 import { ArrowLeft } from "phosphor-react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "./ScreenshotButton";
@@ -6,13 +7,26 @@ import { ScreenshotButton } from "./ScreenshotButton";
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
     onFeedbackRestartRequested: () => void;
+    onFeedbackSent: () => void;
 }
 
 export function FeedbackContentStep({
     feedbackType,
     onFeedbackRestartRequested,
+    onFeedbackSent
 }: FeedbackContentStepProps) {
+    const [screenShot, setScreenShot] = useState<string | null>(null)
+    const [comment, setComment] = useState('')
     const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+    function handleSubmitFeedback(event: FormEvent){
+        event.preventDefault()
+        console.log({
+            screenShot,
+            comment
+        })
+        onFeedbackSent();
+    }
     return (
         <>
             <header>
@@ -34,16 +48,21 @@ export function FeedbackContentStep({
                 <CloseButton />
             </header>
 
-            <form className="my-4 w-full">
+            <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
                 <textarea
                     className="min-w-[340px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-50 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                     placeholder="Conte com detalhes o que está acontecendo..."
+                    onChange={event => setComment(event.target.value)}
                 />
                 <footer className="flex gap-2 mt-2">
-                    <ScreenshotButton />
+                    <ScreenshotButton
+                        screenshot={screenShot}
+                        onScreenshotTook={setScreenShot}
+                    />
                     <button
                         type="submit"
-                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                        disabled={comment.length === 0}
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                     >
                         Enviar Feedback
                     </button>
--- web/src/components/WidgetForm/Steps/ScreenshotButton.tsx
+++ web/src/components/WidgetForm/Steps/ScreenshotButton.tsx
@@ -1,16 +1,38 @@
-import { Camera } from "phosphor-react";
+import { Camera, Trash } from "phosphor-react";
 import html2canvas from 'html2canvas'
 import { useState } from "react";
 import { Loading } from "../../Loading";
 
-export function ScreenshotButton() {
+interface ScreenshotButtonProps {
+    screenshot: string | null;
+    onScreenshotTook: (screenshot: string | null) => void
+}
+
+export function ScreenshotButton({ screenshot, onScreenshotTook }: ScreenshotButtonProps) {
     const [isTakingScreenShot, setIsTakinScreenShot] = useState(false)
 
     async function handleTakeScreenshot(){
         setIsTakinScreenShot(true)
         const canvas = await html2canvas(document.querySelector('html')!)
         const base64image = canvas.toDataURL('image/png')
+        onScreenshotTook(base64image)
         setIsTakinScreenShot(false)
+    }
+    if (screenshot){
+        return(
+            <button
+                type='button'
+                className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+                style={{
+                    backgroundImage: `url(${screenshot})`,
+                    backgroundPosition: 'right bottom',
+                    backgroundSize: 180
+                }}
+                onClick={()=> onScreenshotTook(null)}
+            >
+                <Trash weight="fill" />
+            </button>
+        )
     }
     return (
         <button
